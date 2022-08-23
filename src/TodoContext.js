64e3f23036861104051073c8e781378404841b58
dulce04 <aocl333@nate.com>
2022-08-24 00:16:56 +0900

    todolist

--- src/TodoContext.js
+++ src/TodoContext.js
@@ -3,22 +3,22 @@ import React, { useReducer, createContext, useContext, useRef } from "react";
 const initialTodos = [
   {
     id: 1,
-    text: "프로젝트 생성하기",
+    text: "1일 1커밋",
     done: true,
   },
   {
     id: 2,
-    text: "컴포넌트 스타일링하기",
+    text: "리액트 공부하기",
     done: true,
   },
   {
     id: 3,
-    text: "Context 만들기",
+    text: "알코리즘 강의 듣기",
     done: false,
   },
   {
     id: 4,
-    text: "기능 구현하기",
+    text: "프로젝트 구상하기",
     done: false,
   },
 ];
